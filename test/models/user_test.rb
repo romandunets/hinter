45560a806b1f2cc54919378f21f81a74b854a33f
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-25 17:47:51 +0200

    Add unit tests for user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(email: "user@example.com", password: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 250 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user.@example.com@example.com user@exam+ple.com user@example,com user_at_example.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    second_user = @user.dup
+    second_user.email = @user.email.upcase
+    @user.save
+    assert_not second_user.valid?
+  end
+
+  test "email addresses should be downcase" do
+    email = "USER@EXAMPLE.COM"
+    @user.email = email
+    @user.save
+    assert_equal email.downcase, @user.reload.email
+  end
+
+end
