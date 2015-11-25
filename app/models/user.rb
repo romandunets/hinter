4446910f3c455549e7e5d3fe5d97ba7aab4520a5
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-25 17:47:27 +0200

    Add validation for email attribute in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
 
   has_secure_password
 end
