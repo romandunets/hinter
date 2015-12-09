baa0fa3d3bb27fb3dd06ba6c9281a18acb2758f1
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-09 19:15:33 +0200

    Add log_in in create user action to login user automatically after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Your account was successfully created."
       redirect_to @user
     else
