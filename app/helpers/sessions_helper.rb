b2d4f2d99a983610935dedd3aced95becab3adff
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-15 19:49:52 +0200

    Add method in sessions helper for redirecting back or to default page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,4 +40,9 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 end
