76f9cc2d8d295fe22535b629b56216a062193817
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-16 23:03:36 +0200

    Add index action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   layout "static", only: [:new, :create]
 
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
