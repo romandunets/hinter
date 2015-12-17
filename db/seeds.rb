e987e7517974e7f688a73f466c265043a162eb2b
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-17 20:40:30 +0200

    Add user model seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: "test@mail.com", password: "password", password_confirmation: "password")
+
+99.times do |n|
+  email = "test-#{n+1}@mail.com"
+  password = "password"
+  User.create!(email: email, password: password, password_confirmation: password)
+end
