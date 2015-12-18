7ea064bdd20396c054196e1f5dbd030895a003d8
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-18 19:13:24 +0200

    Add is_admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210160238) do
+ActiveRecord::Schema.define(version: 20151218165935) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151210160238) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_digest"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
