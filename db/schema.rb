65374a8919778391be62e1d375ae45e53c94d6ea
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-22 18:29:19 +0200

    Add reset password columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219203858) do
+ActiveRecord::Schema.define(version: 20151222162758) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20151219203858) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
