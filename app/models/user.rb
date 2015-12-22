965414564eb329c2b8d2002013641d009d4032fe
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-22 18:17:13 +0200

    Refactor user account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,15 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     def downcase_email
