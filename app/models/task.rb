ea5f19f21f0988546a7c34a519ab03d0534ab8c3
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-26 21:56:35 +0200

    Add default scope for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 250 }
+
+  default_scope -> { order(created_at: :desc) }
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,7 +4,8 @@ class TaskTest < ActiveSupport::TestCase
 
   def setup
     @user = User.create(email: "user@example.com", password: "password")
-    @task = @user.tasks.build(title: "Update README.md", description: "Update README.md file", priority: 2, state: "In progress", deadline: DateTime.now + 7.days, user_id: @user.id)
+    @task = @user.tasks.create!(title: "Update README.md", description: "Update README.md file", priority: 2, state: "In progress", deadline: DateTime.now + 7.days, user_id: @user.id)
+    @closest_task = @user.tasks.create!(title: "Add controller", description: "Add code for controller", priority: 2, state: "Open", deadline: DateTime.now + 1.days, user_id: @user.id)
   end
 
   test "should be valid" do
@@ -19,6 +20,10 @@ class TaskTest < ActiveSupport::TestCase
   test "title should be present" do
     @task.title = " "
     assert_not @task.valid?
+  end
+
+  test "order should be by deadline" do
+    assert_equal @closest_task, Task.first
   end
 
 end
