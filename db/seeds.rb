47d7851930f6a957332df2ee1c6530c68827c7ed
Roman Dunets <roman.dunets.email@gmail.com>
2015-12-27 22:01:15 +0200

    Add seeds for tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@ User.create!(email: "test@mail.com", password: "password", password_confirmation
   password = "password"
   User.create!(email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  title = Faker::Lorem.sentence(1)
+  description = Faker::Lorem.sentence(5)
+  users.each { |user| user.tasks.create!(title: title, description: description, priority: 2, state: "Open", deadline: DateTime.now + 7.days, user_id: user.id) }
+end
