bfdc3547304a6d2c4d2b1f48be88e6c491fe93f5
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-01 19:24:54 +0200

    Fine-tune task list

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,15 @@ module TasksHelper
   def distance_to_deadline_in_words(deadline)
     "#{distance_of_time_in_words(DateTime.now, deadline)} #{(DateTime.now > deadline) ? ' late' : ' left' }"
   end
+
+  def priority_in_words(priority)
+    case priority
+    when 1
+      "Low"
+    when 2
+      "Medium"
+    when 3
+      "High"
+    end
+  end
 end
