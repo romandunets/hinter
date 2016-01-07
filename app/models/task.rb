027128276fa9303dcea055f55f21f67cb91d40d7
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-07 22:27:50 +0200

    Add validation for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,11 @@ class Task < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 250 }
+  validates :priority, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :deadline, presence: true
+  validates :user, presence: true
+
+  validate :validate_deadline
 
   default_scope -> { order(:deadline) }
 
@@ -14,4 +19,10 @@ class Task < ActiveRecord::Base
       transition [:open, :in_progress] => :done
     end
   end
+
+  private
+
+    def validate_deadline
+      errors.add(:deadline, "can't be in the past") if deadline.present? and deadline < DateTime.now
+    end
 end
