a706e4f90efdf7b49f1b4447b5a86c1054f31882
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-08 21:22:22 +0200

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103111703) do
+ActiveRecord::Schema.define(version: 20160108191103) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20160103111703) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "first_name"
+    t.string   "second_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
