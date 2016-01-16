2d5281fa6dab9a1126584150028df4080f51ac51
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-16 22:59:01 +0200

    Fix task helper with translations

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,20 +5,24 @@ module TasksHelper
 
   def distance_to_deadline_in_words(task)
     if task.done?
-      "Closed #{distance_of_time_in_words(DateTime.now, task.closed_at)} ago"
+      I18n.t("tasks.messages.closed", distance: distance_of_time_in_words(DateTime.now, task.closed_at))
     else
-      "#{distance_of_time_in_words(DateTime.now, task.deadline)} #{(DateTime.now > task.deadline) ? ' late' : ' left' }"
+      if DateTime.now > task.deadline
+        I18n.t('tasks.messages.late', distance: distance_of_time_in_words(DateTime.now, task.deadline))
+      else
+        I18n.t('tasks.messages.left', distance: distance_of_time_in_words(DateTime.now, task.deadline))
+      end
     end
   end
 
   def priority_in_words(priority)
     case priority
     when 1
-      "Low"
+      I18n.t('tasks.priorities.low')
     when 2
-      "Medium"
+      I18n.t('tasks.priorities.medium')
     when 3
-      "High"
+      I18n.t('tasks.priorities.high')
     end
   end
 end
